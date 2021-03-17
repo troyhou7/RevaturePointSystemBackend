e539adf1105146e10280286186c54d6bc2d8e548
Troy Houston <35402866+troyhou7@users.noreply.github.com>
2021-03-17 17:55:55 -0400

    fixed commit differences

--- RevaturePointsAPI/src/test/java/dev/group3/RevaturePointsAPI/PrizeServiceTests.java
+++ RevaturePointsAPI/src/test/java/dev/group3/RevaturePointsAPI/PrizeServiceTests.java
@@ -1,7 +1,7 @@
 package dev.group3.RevaturePointsAPI;
 
 import dev.group3.entities.Prize;
-<<<<<<< Updated upstream
+
 import dev.group3.repos.PrizeRepo;
 import dev.group3.services.PrizeService;
 
@@ -9,18 +9,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
 
 import java.util.HashSet;
 import java.util.Set;
 
-@SpringBootTest
-@ExtendWith(MockitoExtension.class)
-=======
 import dev.group3.services.PrizeService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,41 +24,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
->>>>>>> Stashed changes
 public class PrizeServiceTests {
 
     @Autowired
     PrizeService prizeService;
 
-<<<<<<< Updated upstream
-    @MockBean
-    PrizeRepo prizeRepo;
-
-    @BeforeEach
-    void setUp(){
-        Prize p1 = new Prize(1,"Trip",1000000,"Trip to Hawaii");
-        Prize p2 = new Prize(2,"Candle",100,"A nice smelling candle");
-        Prize p3 = new Prize(3,"TV",10000,"A 40 inch 4k TV!");
-        Prize p4 = new Prize(4,"Car",200000,"Honda Civic");
-
-        Set<Prize> prizes = new HashSet<>();
-
-        prizes.add(p1);
-        prizes.add(p2);
-        prizes.add(p3);
-        prizes.add(p4);
-
-        System.out.println(prizeRepo);
-        Mockito.when(prizeRepo.findAll()).thenReturn(prizes);
-    }
-
-    @Test
-    void get_all_prizes(){
-        Set<Prize> prizes = this.prizeService.getAllPrizes();
-        System.out.println(prizes);
-    }
-
-=======
     static int prizeId;
 
     @Test
@@ -100,8 +66,5 @@ public class PrizeServiceTests {
         boolean deleted = this.prizeService.deletePrizeById(prizeId);
         Assertions.assertTrue(deleted);
     }
->>>>>>> Stashed changes
-
-
 
 }
