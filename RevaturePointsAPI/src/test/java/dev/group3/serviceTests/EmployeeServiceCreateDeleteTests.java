e04f0f18897a2a66aaedb584a388ce17321a91e3
MGlennUA <62678834+MGlennUA@users.noreply.github.com>
2021-03-18 14:52:26 -0700

    Create EmployeeServiceCreateDeleteTests.java
    wrote tests for EmployeeService mehtods:
    registerEmployee
    updateEmployee
    deleteEmployeeById

---
+++ RevaturePointsAPI/src/test/java/dev/group3/serviceTests/EmployeeServiceCreateDeleteTests.java
@@ -0,0 +1,51 @@
+package dev.group3.serviceTests;
+
+import dev.group3.RevaturePointsAPI.RevaturePointsApiApplication;
+import dev.group3.entities.Employee;
+import dev.group3.services.EmployeeService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
+
+@SpringBootTest(classes= RevaturePointsApiApplication.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EmployeeServiceCreateDeleteTests {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    static Employee testEmployee;
+
+    @Test
+    @Order(1)
+    void registerEmployeeTest() {
+        testEmployee = new Employee(0,"Associate", "Parker", "Hoskovec", "PHoskovec", "password", 0, 0, 1);
+
+        employeeService.registerEmployee(testEmployee);
+
+        Assertions.assertNotEquals(0, testEmployee);
+    }
+
+    @Test
+    @Order(2)
+    void updateEmployeeTest() {
+        testEmployee.setFname("TestName");
+
+        Employee updated = employeeService.updateEmployee(testEmployee);
+
+        Assertions.assertNotNull(updated);
+        Assertions.assertTrue(updated.getFname().equals("TestName"));
+    }
+
+    @Test
+    @Order(3)
+    void deleteEmployeeByIdTest() {
+        Assertions.assertTrue(employeeService.deleteEmployeeById(testEmployee.getEmployeeId()));
+
+        Employee employee = employeeService.getEmployeeById(testEmployee.getEmployeeId());
+
+        Assertions.assertNull(employee);
+    }
+}
